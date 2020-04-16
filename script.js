1b40724a228b17a210213ac52ea043478b496950
Cattleya Pia <cjpia612@gmail.com>
2020-04-15 23:46:19 -0700

    added functioning conditional statement and some pseudocode

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var criteriaPw = 
-  "special": ["~","!","@","#","$","%","^","&","*","(",")","-","_","+","="]
-  "uppers":["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O",
-            "P","Q","R","S","T","U","V","W","X","Y","Z"]
-  "lowers":["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o",
-            "p","q","r","s","t","u","v","w","x","y","z"]
-  "numbers":["0","1","2","3","4","5","6","7","8","9"]
+// var criteriaPw = 
+//   "special": ["~","!","@","#","$","%","^","&","*","(",")","-","_","+","="]
+//   "uppers":["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O",
+//             "P","Q","R","S","T","U","V","W","X","Y","Z"]
+//   "lowers":["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o",
+//             "p","q","r","s","t","u","v","w","x","y","z"]
+//   "numbers":["0","1","2","3","4","5","6","7","8","9"]
   
 // Write password to the #password input
 function writePassword() {
@@ -18,11 +18,18 @@ function writePassword() {
 }
  
 // Add event listener to generate button
-generateBtn.addEventListener("click", function(){
-  parseInt(prompt("Choose length of password between 8-128 characters."));
-});
+generateBtn.addEventListener("click", 
+function(){
+ var pwLength = parseInt(prompt("Choose length of password between 8-128 characters."));
+ if (pwLength < 8 || pwLength > 128) {
+    alert("Invalid input. Please refresh page and start again");
+ } 
+  });
 
-// var charAmount = prompt ("Choose length of password between 8-128 characters.");
+//else if (pwLength === 8 || pwLength > 8 || pwLength < 128 || pwLength === 128) {
+//
+// }
+// }
 
 
 // var charAmount = []
