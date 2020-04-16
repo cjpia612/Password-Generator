655208d93243d3113d2570f65f3bf965c0cd0b2e
Cattleya Pia <cjpia612@gmail.com>
2020-04-15 22:49:10 -0700

    created criteria arrays, added parseInt to first prompt

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
+var criteriaPw = 
+  "special": ["~","!","@","#","$","%","^","&","*","(",")","-","_","+","="]
+  "uppers":["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O",
+            "P","Q","R","S","T","U","V","W","X","Y","Z"]
+  "lowers":["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o",
+            "p","q","r","s","t","u","v","w","x","y","z"]
+  "numbers":["0","1","2","3","4","5","6","7","8","9"]
+  
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -9,17 +16,21 @@ function writePassword() {
   passwordText.value = password;
 
 }
-
+ 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", function(){
+  parseInt(prompt("Choose length of password between 8-128 characters."));
+});
+
+// var charAmount = prompt ("Choose length of password between 8-128 characters.");
 
 
-function clickButton(){
-    var generate = prompt( "Choose length of password between 8-128 characters.");
-}
-// if{
-//     prompt= ("Choose length of password between 8-128 characters.");
+// var charAmount = []
+// if (generate) {
+//     prompt( "Do you want lower case letters?")
 // }
-// if (generateBtn.addEventListener) {
-//     alert ("Choose from the following password criteria.");
-// }
+// // Loop 10 times
+// for (var i = 0; i < 10; i++) {
+//     // Generate a random number between 1 and 10
+//     // Math.floor will round down, meaning we would get a number between 0 and 9, so we'll always add 1 to bump it up.
+//     var num = Math.floor(Math.random() * 10) + 1;
